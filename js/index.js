a087bad0c0bb7be478af3faf3e5c72ca46cbd25b
thiri sandar <thiris613@gmail.com>
2024-08-27 23:05:21 +0630

    M feedback M contact

--- js/index.js
+++ js/index.js
@@ -51,5 +51,40 @@ function generatePopularProductCards(data) {
         `;
         productSection.appendChild(productCard);
     });
+
 }
 
+// google chart
+  // Load the Visualization API and the corechart package.
+  google.charts.load('current', {'packages':['corechart']});
+
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.charts.setOnLoadCallback(drawChart);
+
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+  function drawChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+      ['Americano', 3],
+      ['Latte', 1],
+      ['Affogato', 1],
+      ['Mocha', 1],
+      ['Nitro Coffee', 2]
+    ]);
+
+    // Set chart options
+    var options = {'title':'Best Coffees based on rating',
+                   'width':600,
+                   'height':400};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    chart.draw(data, options);
+
+}
